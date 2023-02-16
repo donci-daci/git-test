b098b4de63fd02e08ecdf5915d8c2ddb65063ef7
mingao <1783339517@qq.com>
2023-02-16 17:20:02 +0800

    第一次初始化本地库并提交

---
+++ src/main/java/gitHot.java
@@ -0,0 +1,6 @@
+public class gitHot {
+    public static void main(String[] args) {
+        System.out.println("Hello Hot-fix");
+    }
+}
+
---
+++ src/main/java/gitTest.java
@@ -0,0 +1,5 @@
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("第一次初始化git本地仓库");
+    }
+}
