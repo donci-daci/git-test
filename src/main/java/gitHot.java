846b873355f0e7c42cdc141dfc9d02fa5c64c4fc
mingao <1783339517@qq.com>
2023-02-16 17:30:23 +0800

    hot-fix分支第一次提交

--- src/main/java/gitHot.java
+++ src/main/java/gitHot.java
@@ -1,6 +1,10 @@
+/**
+ * @author ma
+ * @create 2023-02-16 17:25
+ */
 public class gitHot {
     public static void main(String[] args) {
-        System.out.println("Hello Hot-fix");
+
+        System.out.println("hot-fix第一次修改");
     }
 }
-
